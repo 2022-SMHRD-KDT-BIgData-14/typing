be5d13dc1c33c05830924fb3e2fdfddb246b2f7c
smhrd <smhrd@59.0.147.198>
2022-05-31 14:53:02 +0900

    게임디에이오추가

--- teamp/src/gameplay/GameOption.java
+++ teamp/src/gameplay/GameOption.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+import user.userDTO;
+
 public class GameOption {
-	Connection conn;
-	PreparedStatement psmt;
-	ResultSet rs;
 	Scanner sc = new Scanner(System.in);
 	Random r = new Random();
+	
 	
 	
 //타이핑 게임
@@ -43,47 +43,6 @@ public class GameOption {
 			return 0;
 		}
 	}
-
-	// 케릭터 정보 가져오기
-	public int characterStat(int num) {
-		String sql = "select * from character";
-		String att = "";
-		String def = "";
-		String hp = "";
-		try {
-			psmt = conn.prepareStatement(sql);
-			rs = psmt.executeQuery();
-//			System.out.print("ID" + "\t");
-//			System.out.print("NICNAME" + "\t");
-			System.out.print("ATT" + "\t");
-			System.out.print("DEF" + "\t");
-			System.out.print("HP" + "\t");
-			System.out.println("");
-			while (rs.next()) {
-				att = rs.getString(1);
-				def = rs.getString(2);
-				hp = rs.getString(3);
-				System.out.print(att + "\t");
-				System.out.print(def + "\t");
-				System.out.println(hp);
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		int attack = Integer.parseInt(att);
-		int defend = Integer.parseInt(def);
-		int health = Integer.parseInt(hp);
-		if (num == 1) {
-			return attack;
-		} else if (num == 2) {
-			return defend;
-		} else if (num == 3) {
-			return health;
-		} else {
-			return 0;
-		}
-
-	}
+	
 
 }
