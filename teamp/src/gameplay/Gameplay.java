d74292b26341f5868b0097010147a1d2c9250f3f
smhrd <smhrd@119.200.31.122>
2022-06-02 11:17:21 +0900

    상익 : 소스폴더에 음악추가, main과 gameplay 클래스에 음악 주가

--- teamp/src/gameplay/Gameplay.java
+++ teamp/src/gameplay/Gameplay.java
@@ -3,6 +3,7 @@ package gameplay;
 import java.util.Scanner;
 
 import character.CharacterDTO;
+import javazoom.jl.player.MP3Player;
 import rank.rankViewDAO;
 
 public class Gameplay {
@@ -12,6 +13,7 @@ public class Gameplay {
 		GameDAO dao = new GameDAO();
 		Scanner sc = new Scanner(System.in);
 		dao.connect();
+		MP3Player mp3 = new MP3Player();
 	
 		int[] bossHp = {10000, 15000,20000, 25000, 30000, 35000, 40000, 45000, 50000,55000};
 		int bossAtt = 5;
@@ -24,10 +26,13 @@ public class Gameplay {
 		dao.close();
 
 		
-		
-		
 // 타이핑 공격
 		while (true) {
+			if(mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(".\\music\\게임진행1_사용자턴 배경음.mp3");
+			
 			System.out.println("ROUND"+round++);
 			System.out.println("============================================");
 			int cnt =0;
@@ -63,11 +68,21 @@ public class Gameplay {
 				
 			}
 			if (heroHp <= 0) {
+				if(mp3.isPlaying()) {
+					mp3.stop();
+				}
 				System.out.println("게임이 종료되었습니다");
+				mp3.play(".\\music\\게임종료화면 배경음.mp3");
 				break;
 			}
 			i++;
 			System.out.println("=============="+round+"클리어==============");
+			
+			if(mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(".\\music\\게임진행3_라운드상승 배경음.mp3");
+			
 			System.out.println("[1]다음 라운드    [2]종료");
 			int input =sc.nextInt();
 			if(input==2) {
--- teamp/src/teamp/main.java
+++ teamp/src/teamp/main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import character.CharacterDTO;
 import gameplay.GameOption;
 import gameplay.Gameplay;
+import javazoom.jl.player.MP3Player;
 import rank.rankDAO;
 import rank.rankDTO;
 import rank.rankViewDAO;
@@ -22,9 +23,16 @@ public class main {
 		GameOption option = new GameOption();
 		int num;
 		String user_id = "";
-
+		MP3Player mp3 = new MP3Player();
+		
+		if(mp3.isPlaying()) {
+			mp3.stop();
+		}
 		// 초기 화면
 		while (true) {
+			
+			mp3.play(".\\Music\\시작화면 배경음.mp3");
+			
 			System.out.println("[1]회원가입 [2] 로그인 [3] 종료");
 			num = sc.nextInt();
 
@@ -51,14 +59,28 @@ public class main {
 		}
 		// 로그인 후 화면
 		while (num == 2) {
+			if(mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(".\\music\\메뉴선택시 효과음.mp3");
 			System.out.println("[1]게임시작 [2] 랭킹조회 [3]종료");
 			num = sc.nextInt();
+			
+			
 			if (num == 1) {
 
+				if(mp3.isPlaying()) {
+					mp3.stop();
+				}
+				mp3.play(".\\music\\캐릭터생성화면 배경음.mp3");
+				
 				// 캐릭터 생성
 				cdto = option.characterProduct(num, user_id);
 
 				// 게임 실행
+				if(mp3.isPlaying()) {
+					mp3.stop();
+				}
 				int round = play.gamePlay(cdto);
 
 				System.out.println("랭킹을 등록 하시겠습니까");
@@ -67,22 +89,45 @@ public class main {
 				if (num == 1) {
 					rankDTO rankdto = new rankDTO(cdto.getnickname(), round);
 					rankdao.rankInsert(rankdto);
+					
 					System.out.println("랭킹등록 완료");
+					if(mp3.isPlaying()) {
+						mp3.stop();
+					}
+					
 				} else if (num == 2) {
 					System.out.println("랭킹등록창 종료 합니다.");
+					if(mp3.isPlaying()) {
+						mp3.stop();
+					}
+					
 				} else {
 					System.out.println("잘못 입력 하셨습니다.");
+					if(mp3.isPlaying()) {
+						mp3.stop();
+					}
+					
 				}
 				num = 2;
 
 			}
 			// 랭킹 조회
 			else if (num == 2) {
+				if(mp3.isPlaying()) {
+					mp3.stop();
+				}
+				mp3.play(".\\music\\랭킹조회화면 배경음.mp3");
+				
 				rankview.rankView();
 				num = 2;
 			}
 			// 게임 종료
 			else if (num == 3) {
+				if(mp3.isPlaying()) {
+					mp3.stop();
+				}
+				mp3.play(".\\music\\게임종료화면 배경음.mp3");
+				
 				view.Exit();
 				break;
 			}
